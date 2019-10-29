3df0e70b17651195e6257d7c88e10f622253ede7
shinohara15296 <42565809+shinohara15296@users.noreply.github.com>
2019-10-29 20:34:24 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -5,15 +5,15 @@ from datetime import datetime, timedelta
 client = discord.Client()
 
 @client.event
-@asyncio.coroutine on_voice_state_update(member, before, after): 
+async def on_voice_state_update(member, before, after): 
     if member.guild.id == 600978911761006593 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
         alert_channel = client.get_channel(600978912809451551)
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
-            yield from alert_channel.send(msg)
+            await from alert_channel.send(msg)
         elif after.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
-            yield from alert_channel.send(msg)
+            await from alert_channel.send(msg)
 
 client.run("NjM4NjYyMTA3OTQ4MTIyMTIy.Xbf-oQ.2moqWsvlCK7hrfQ5lilA_BiO6B0")
